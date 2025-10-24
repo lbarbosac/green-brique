e8d122363a22c1680b2ff2b7c9795808c678aebe
Nicolas <nico.hhara@gmail.com>
2025-10-24 09:39:36 -0300

    24.10 (Sobre nós)
    falta inserir as cores corretas

---
+++ sobreNos/sobreNos.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Sobre Nós</title>
+  <link rel="stylesheet" href="./sobreNos.css">
+</head>
+
+<body>
+  <menu>
+    <div class="menu-container-title">
+      <h1 class="menu-title">Sobre Nós</h1>
+    </div>
+
+    <!-- ======= CARROSSEL ======= -->
+    <div class="carousel-wrapper">
+      <button class="carousel-btn prev">❮</button>
+
+      <div class="carousel">
+        <div class="carousel-images">
+          <img src="https://m.magazineluiza.com.br/a-static/420x420/mascara-macaca-macaco-animal-carnaval-festas-spook-elastico/magiccenter/11546/4880acba1a5f71bd94ee1ec057d87b7a.jpg" alt="Eduardo Avila">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxJNsyJtBYI0nWnDpMXv38LO0rc4G_1AAJkg&s" alt="Nicolas Hara">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/4/4e/Macaca_nigra_self-portrait_large.jpg" alt="Lucas Barbosa">
+          <img src="https://rihappy.vtexassets.com/arquivos/ids/8457168/17515702501622.jpg?v=638872412174200000" alt="Cassiano Ramos">
+        </div>
+
+        <div class="carousel-indicators">
+          <div class="indicator active" data-index="0"></div>
+          <div class="indicator" data-index="1"></div>
+          <div class="indicator" data-index="2"></div>
+          <div class="indicator" data-index="3"></div>
+        </div>
+      </div>
+      <button class="carousel-btn next">❯</button>
+    </div>
+  </menu>
+
+  <main>
+    <h2 class="main-title1">Quem Somos</h2>
+    <p class="main-paragraph1">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia laboriosam, atque aspernatur quibusdam amet suscipit fugiat. Molestiae sapiente doloremque repellendus facilis velit non dolores veniam, ea, ipsa ad fuga ullam!</p>
+
+    <h2 class="main-title2">Nossa Missão</h2>
+    <p class="main-paragraph2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque molestias dolorem atque repellat enim incidunt ut. Earum exercitationem dolor id, culpa error, nisi blanditiis architecto doloribus, minus tempora odit sit.</p>
+  </main>
+
+  <footer>
+    <div class="footer-container1">
+      <img src="" alt="" class="footer-img">
+      <h3 class="footer-title">Sustentabilidade</h3>
+      <p class="footer-paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum consequatur mollitia ea fugiat. Dicta similique doloribus blanditiis veritatis, accusantium delectus! Est quis, fuga consectetur amet dicta voluptas. A, deleniti quae!</p>
+    </div>
+
+    <div class="footer-container2">
+      <img src="" alt="" class="footer-img">
+      <h3 class="footer-title">Saúde</h3>
+      <p class="footer-paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi asperiores voluptatem earum reprehenderit, similique odit in voluptates quidem magni, delectus, sapiente rerum consequatur exercitationem. Molestias dicta dolores nam hic earum!</p>
+    </div>
+
+    <div class="footer-container3">
+      <img src="" alt="" class="footer-img">
+      <h3 class="footer-title">Economia</h3>
+      <p class="footer-paragraph">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita, rem odit quam alias natus incidunt, officia nostrum, minima totam quasi quas id numquam laboriosam sint corporis nihil saepe? Tenetur, delectus.</p>
+    </div>
+  </footer>
+
+  <script src="./sobreNos.js"></script>
+</body>
+</html>
