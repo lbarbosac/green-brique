02f70e300061e9bf3afdbaab82ebb2a821f83d49
Nicolas <nico.hhara@gmail.com>
2025-11-17 08:58:30 -0300

    Page visualizao comerciante

--- src/perfil-comerciante-visualizacao.php
+++ src/perfil-comerciante-visualizacao.php
@@ -3,12 +3,24 @@
     include_once './include/head.php';
 
 
-    $sql = "SELECT ProdutoID, Nome, Descricao, Preco, Quantidade, Img FROM produtos WHERE ProdutoID = " . $produto_id;
+    $sql = "SELECT Nome, Telefone, Endereco,Foto, Maps,Telefone FROM comerciantes WHERE ComercianteID = " . $comerciante_id;
     $retorno = mysqli_query($conn, $sql);
 
-    $nome_comerciante = "Comerciante Exemplo";
-    $endereco_comerciante = "Rua Exemplo, 123, Cidade, Estado";
-    $img_maps = "https://t2.tudocdn.net/744441?w=824&h=494";
+    if($retorno && mysqli_num_rows($retorno) > 0) {
+        $produto = mysqli_fetch_assoc($retorno);
+        
+        $foto_comerciante = htmlspecialchars($produto['Foto']);
+        $nome_comerciante = htmlspecialchars($produto['Nome']);
+        $endereco_comerciante = htmlspecialchars($produto['Endereco']);
+        $img_maps_comerciante = htmlspecialchars($produto['Maps']);
+        $telefone_comerciante = htmlspecialchars($produto['Telefone']);
+    } else {
+        $foto_comerciante = "Foto não Disponível";
+        $nome_comerciante = "Comerciante não Encontrado";
+        $endereco_comerciante = "Endereço não Disponível";
+        $img_maps_comerciante = "Localização não Disponível";
+        $telefone_comerciante = "Contato não Disponível";
+    }
 ?>
 
 <!DOCTYPE html>
@@ -23,21 +35,31 @@
     <main>
         <section class="perfil-comerciante">
             <div class="container-perfil-comerciante">
+
                 <div class="foto-perfil-comerciante">
-                    <img src="#" alt="Foto de Perfil do Comerciante">
+                    <img class="img-perfil" src="<?php echo ($foto_comerciante)?>" alt="Foto de Perfil do Comerciante">
                 </div>
+
                 <div class="informacoes-comerciante">
                     <p><strong>Nome do Comerciante:</strong>
                         <?php
                             echo ($nome_comerciante);
                         ?>
                     </p>
-                    <p><strong>Endereço:</strong> Rua Exemplo, 123, Cidade, Estado</p>
+
+                    <p><strong>Endereço:</strong>
+                        <?php
+                            echo ($endereco_comerciante);
+                        ?>
+                    </p>
                     <div class="container-maps">
-                        <img src="https://t2.tudocdn.net/744441?w=824&h=494" alt="">
+                        <img class="img-maps" src="<?php echo ($img_maps_comerciante); ?>" alt="Mapa do comerciante">
                     </div>
-                    <p><strong>Contato:</strong> (11) 1234-5678</p>
-                    <p><strong>Descrição:</strong> Breve descrição sobre o comerciante e seus serviços.</p>
+                    <p><strong>Contato:</strong>
+                        <?php
+                            echo ("$telefone_comerciante");
+                        ?>
+                    </p>
                 </div>
             </div>
         </section>
